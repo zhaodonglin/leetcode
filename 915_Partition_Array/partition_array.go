4cbf65a3e5c0e558bc910eaab39445ec970b8a10
萍朱 <pingzhu@pingzhudeMBP.hitronhub.home>
2018-10-01 10:26:53 -0600

    modify compile error

--- 915_Partition_Array/partition_array.go
+++ 915_Partition_Array/partition_array.go
@@ -1,9 +1,7 @@
 package partition_array
 
-//10:10
-
 func getMin(right []int) int {
-	min = right[0]
+	min := right[0]
 
 	for i := 0; i < len(right); i++ {
 		if min > right[i] {
@@ -17,16 +15,19 @@ func getMin(right []int) int {
 func isLeftLessThanRight(left []int, right []int) bool {
 	min := getMin(right)
 	for i := 0; i < len(left); i++ {
+
 		if left[i] > min {
 			return false
 		}
 	}
+	return true
 }
 
 func partitionDisjoint(A []int) int {
-	for i := 1; i < len(A)-1; i++ {
+	for i := 1; i <= len(A)-1; i++ {
 		if isLeftLessThanRight(A[:i], A[i:]) {
 			return i
 		}
 	}
+	return -1
 }
--- 915_Partition_Array/partition_array_test.go
+++ 915_Partition_Array/partition_array_test.go
@@ -12,4 +12,8 @@ func Test_Partition_Array(t *testing.T) {
 	if 4 != partitionDisjoint([]int{1, 1, 1, 0, 6, 12}) {
 		t.Error("failed")
 	}
+
+	if 1 != partitionDisjoint([]int{1, 1}) {
+		t.Error("failed")
+	}
 }
