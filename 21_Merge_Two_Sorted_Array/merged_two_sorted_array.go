7f733dbd7cd377efd64575018857e024683335e5
萍朱 <pingzhu@pingzhudeMacBook-Pro.local>
2018-10-16 19:14:22 -0600

    batch commit

--- 21_Merge_Two_Sorted_Array/merged_two_sorted_array.go
+++ 21_Merge_Two_Sorted_Array/merged_two_sorted_array.go
@@ -1,1 +1,59 @@
 package merged_two_sorted_array
+
+import "fmt"
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func traverse(head *ListNode) {
+	p1 := head
+	fmt.Println("begin:")
+	for p1 != nil {
+		fmt.Println(p1.Val)
+		p1 = p1.Next
+	}
+	fmt.Println("end")
+}
+
+func append(head *ListNode, node *ListNode) {
+	node.Next = head.Next
+	head.Next = node
+}
+
+func mergeTwoLists(L1 *ListNode, L2 *ListNode) *ListNode {
+	var dummy ListNode
+
+	head1 := L1
+	head2 := L2
+
+	var p1 *ListNode
+	p1 = &dummy
+
+	for head1 != nil && head2 != nil {
+		if head1.Val < head2.Val {
+			p1.Next = head1
+			p1 = head1
+			head1 = head1.Next
+		} else {
+			p1.Next = head2
+			p1 = head2
+			head2 = head2.Next
+		}
+	}
+
+	for head1 != nil {
+		p1.Next = head1
+		p1 = head1
+		head1 = head1.Next
+	}
+
+	for head2 != nil {
+		p1.Next = head2
+		p1 = head2
+		head2 = head2.Next
+	}
+
+	return dummy.Next
+}
--- 21_Merge_Two_Sorted_Array/merged_two_sorted_array_test.go
+++ 21_Merge_Two_Sorted_Array/merged_two_sorted_array_test.go
@@ -1,1 +1,29 @@
 package merged_two_sorted_array
+
+import "testing"
+
+func Test_Merged_Two_Sorted_Array(t *testing.T) {
+	var head ListNode
+	head.Val = 1
+	head.Next = nil
+
+	var listNode1 ListNode
+	listNode1.Val = 4
+
+	var listNode2 ListNode
+	listNode2.Val = 2
+
+	append(&head, &listNode1)
+	append(&head, &listNode2)
+	traverse(&head)
+
+	var head2 ListNode
+	head2.Val = 3
+
+	var listNode3 ListNode
+	listNode3.Val = 5
+	append(&head2, &listNode3)
+	traverse(&head2)
+
+	traverse(mergeTwoLists(&head, &head2))
+}
